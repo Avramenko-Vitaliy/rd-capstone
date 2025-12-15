51202b562cffdd800ef298b5ef38254f01c6c3b3
Vitaliy Avramenko <avramenko_vitaliy@ukr.net>
2025-12-15 11:59:42 +0200

    add TodoDto to encapsulate Todo data with validation constraints

---
+++ rd-api/src/main/java/com/rd/api/dtos/TodoDto.java
@@ -0,0 +1,23 @@
+package com.rd.api.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+
+@Value
+public class TodoDto implements Serializable {
+
+    Integer id;
+
+    @NotBlank
+    String name;
+
+    @NotNull
+    LocalDateTime date;
+
+    String description;
+}
